038ebab1229b2e5166f409160b4c5238291030ca
leeheeweon <ggoddii@kaoni.com>
2023-05-02 06:46:31 +0900

    파이썬 예외처리

--- python1.0_source/chapter05_02_ex1.py
+++ python1.0_source/chapter05_02_ex1.py
@@ -5,12 +5,12 @@
 
 
 # 예제1 -> 예외처리
-# try:
-#     n = int(input('Enter a number: '))
-#     print('OK. Your number is: ', n)
+try:
+    n = int(input('Enter a number: '))
+    print('OK. Your number is: ', n)
 
-# except ValueError:
-#     print('This is not a number.')
+except ValueError:
+    print('This is not a number.')
     
     
 # 예제2 -> 올바른 값 입력 완료 까지 지속
