2bba89deb0dc7e2d67ae7c17b52be03940afa5d1
leeheeweon <ggoddii@kaoni.com>
2023-05-24 07:45:32 +0900

    정보처리기사 실기 파이썬 연습

--- python1.0_source/chapter06_01.py
+++ python1.0_source/chapter06_01.py
@@ -128,5 +128,13 @@ print(c.speak('hu hu'))
 print(d.speak('test'))
 print(d.speak('test2'))
 
+asia = {'한국','중국','일본'}
+asia.add('베트남')
+asia.add('중국')
+asia.remove('일본')
+asia.update(['홍콩','한국','태국'])
+print(asia)
+
+
 
 
