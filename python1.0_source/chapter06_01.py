d2b2d0d5955cbd52542b4d8aac6174f1bf0d2986
leeheeweon <ggoddii@kaoni.com>
2023-07-18 12:00:00 +0900

    Add asset

--- python1.0_source/chapter06_01.py
+++ python1.0_source/chapter06_01.py
@@ -139,6 +139,7 @@ asia = {'한국', '중국', '일본'}
 asia.add('베트남')
 asia.add('중국')
 asia.remove('일본')
+asia.remove('일본')
 asia.update(['홍콩', '한국', '태국'])
 asia.update(['홍콩', '한국', '태국'])
 asia.update(['홍콩', '한국', '태국'])
